80e768bda3270dae34e143f52aafe698d4e6af17
Volker Süß <versus167@gmail.com>
2016-12-10 11:30:29 +0100

    Läuft - Paramterübergabe muss noch erstellt werden

--- zfsnappy.py
+++ zfsnappy.py
@@ -5,13 +5,22 @@ Created on 01.05.2016
 
 Sammlung der commands:
 
+1 - 2016-12-10 - Erste lauffähig Version
+
+Prinzipiell geht es - Die Übergabe der Argumente  ist noch umzuschreiben
+Der Code ist zu säubern...
+
 sudo zfs list -Hp -o avail vs2016/archiv/test - Freier Platz in bytes
 sudo zfs list -Hp -o used vs2016/archiv/test - Genutzer Platz in bytes
 sudo zfs list -H -r -t snapshot -o name,creation vs2016/archiv/test - Liste der Snapshots mit Name und Erstelldatum
 zfs destroy vs2016/archiv/test@hourly_2016-12-10_09.17.01--2d - Snapshot löschen
+zfs snapshot vs2016/archiv/test@vs-2016 - Snapshot erstellen
 
 
 '''
+
+import os
+import datetime
 
 class intervall(object):
     '''
@@ -45,6 +54,8 @@ class intervall(object):
     
 
 if __name__ == '__main__':
+    zfsfs = 'vs2016/archiv/test'
+    snapprefix = 'vs'
     
     inters = []
     inter = intervall(5,3)
@@ -53,11 +64,50 @@ if __name__ == '__main__':
     inters.append(inter)
     inter = intervall(1,5)
     inters.append(inter)
-    for i in range(100,-1,-1):
+    
+    # Hier käme dann der ABlauf
+    # 1. Liste der vorhanden Snapshots
+    aus = os.popen('zfs list -H -r -t snapshot -o name '+zfsfs).readlines()
+    #print(aus) 
+    
+    # 2. Ausdünnen der Liste um die die nicht den richtigen Prefix haben
+    vgl = zfsfs+'@'+snapprefix+'_'
+    l = len(vgl)
+    listesnaps = {}
+    for i in aus:
+        snp = i.strip('\n')
+        if snp[0:l] == vgl:
+            listesnaps[snp] = False
+    #print(listesnaps)
+    heute = datetime.datetime.now()
+    for i in sorted(listesnaps): # Vom ältesten zum neuesten
+        #print(i,listesnaps[i])
+        #erstmal die difftage zu heute ermitteln
+        dstring = i[l:]
+        #print(dstring)
+        dt = datetime.datetime.strptime(dstring,'%Y-%m-%dT%H:%M:%S.%f')
+        tmp = heute - dt
+        chkday = tmp.days 
         hold = False
         for x in inters:
-            if x.checkday(i):
+            if x.checkday(chkday):
                 hold = True
-        print(i,hold)
-    
+        
+        if hold == False:
+            print(i,chkday,hold)
+            cmd = 'zfs destroy '+i
+            print(cmd)
+            aus = os.popen(cmd)
+            for j in aus:
+                print(j)
+        else:
+            print(i,chkday,hold)
+    # Als letztes: Snapshot erstellen
+    aktuell = datetime.datetime.now()
+    snapname = zfsfs+'@'+snapprefix+'_'+aktuell.isoformat() 
+    cmd = 'zfs snapshot '+snapname
+    print(cmd)
+    aus = os.popen(cmd)
+    for j in aus:
+        print(j)
     pass

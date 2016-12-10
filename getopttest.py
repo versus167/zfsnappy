3eb8a7b456d6466e4b505aab2c070c2a11c82a40
Volker Süß <versus167@gmail.com>
2016-12-10 18:06:35 +0100

    opetions

--- getopttest.py
+++ getopttest.py
@@ -8,34 +8,11 @@ from optparse import OptionParser
 
 def main():
     parser = OptionParser()
-    parser.add_option("-i", "--holdinterval",
-                  action="store", dest="inetrvall"
-                  help="don't print status messages to stdout")
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "hoi:v", ["help", "output=",'--holdinterval='])
-    except getopt.GetoptError as err:
-        # print help information and exit:
-        print(err) # will print something like "option -a not recognized"
-        #usage()
-        sys.exit(2)
-    output = None
-    verbose = False
-    for o, a in opts:
-        print(o,a)
-        if o == "-v":
-            verbose = True
-        elif o in ("-h", "--help"):
-            #usage()
-            sys.exit()
-        elif o in ("-o", "--output"):
-            output = a
-        elif o in ('-i','--holdinterval'):
-            print(a)
-            #h,i = a.split(',')
-            #print(h,i)
-        else:
-            assert False, "unhandled option"
-    # ...
+    parser.add_option("-i", "--holdinterval", dest="holds",
+                  help="Holdintervall und Dauer",nargs=2,type='int',action='append')
+    (options,args) = parser.parse_args(sys.argv[1:])
+    print(options)
+
 
 
 if __name__ == '__main__':

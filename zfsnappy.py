d9ddc28c87310dfd01161a0d46c14b5b51ad8cf7
Volker Süß <versus167@gmail.com>
2017-02-19 12:23:08 +0100

    Version 10 mit recursion und com.sun:auto-snapshot=off Behandlung

--- zfsnappy.py
+++ zfsnappy.py
@@ -7,6 +7,8 @@ Created on 10.12.2016
 
 Sammlung der commands:
 
+10 - 2017-02-19 - -r --recursion hinzugefügt - fs die nicht gemounted sind oder die Eigenschaft sun.com:auto-snapshot=off haben
+                werden nicht behandelt
 9 - 2017-02-18 - -v --verbose hinzugefügt - macht zfsnappy etwas gesprächiger - vs.
 8 - 2017-02-13 - -n --nodeletedays eingeführt - snapshots die jünger als diese Anzahl Tage sind, werden nicht gelöscht.
                  Es sei denn minfree wird unterschritten... - vs.
@@ -24,12 +26,12 @@ Todo:
 
 
   - Der Code ist zu säubern...
-  - Rekursion könnte noch verbaut werden
+
   
 
 '''
 APPNAME='zfsnappy'
-VERSION='9 - 2017-02-18'
+VERSION='10 - 2017-02-19'
 
 import os
 import datetime, time
@@ -67,35 +69,35 @@ class intervall(object):
     
 
 def main():
-    def checkfs(fs):
+    def checkfs(fsys):
         ''' Soll schauen, ob das Filesystem auf com.sun:auto-snapshot=off gesetzt ist oder ob es nicht gemountet ist
         - Wenn eines von beiden zutrifft -> kein snapshot - return false '''
-        mounted = os.popen('zfs get -H mounted '+fs).readlines()
+        mounted = os.popen('zfs get -H mounted '+fsys).readlines()
         try:
             if mounted[0].split('\t')[2] == 'yes':
                 # dann alles io
                 pass
             else:
                 if ns.verbose: 
-                    print(fs,'ist nicht gemounted!')
+                    print(fsys,'ist nicht gemounted!')
                 return 1
         except:
             if ns.verbose:
-                print(fs,'ist nicht gemounted!')
+                print(fsys,'ist nicht gemounted!')
             return 1
-        autosnapshot = os.popen('zfs get -H com.sun:auto-snapshot '+fs).readlines()
+        autosnapshot = os.popen('zfs get -H com.sun:auto-snapshot '+fsys).readlines()
         if autosnapshot[0].split('\t')[2] == 'off':
             if ns.verbose:
-                print(fs,'com.sun:auto-snapshot = off')
+                print(fsys,'com.sun:auto-snapshot = off')
             return 2
-        return True
+        return 0
     def checkminfree(tell=False):
         
         
         ''' Erstmal 10 Sekunden warten, damit sich ZFS besinnen kann, wieviel Platz wirklich frei ist -
         falls gerade vorher ein Snapshot gelöscht wurde ''' 
-        avai = os.popen('zfs list -Hp -o avail '+ns.zfsfs).readlines()
-        used = os.popen('zfs list -Hp -o used '+ns.zfsfs).readlines()
+        avai = os.popen('zfs list -Hp -o avail '+fs).readlines()
+        used = os.popen('zfs list -Hp -o used '+fs).readlines()
         #print(avai,used)
         a = int(avai[0].strip('\n'))
         u = int(used[0].strip('\n'))
@@ -111,7 +113,7 @@ def main():
         return True
     def takeSnapshot():
         aktuell = datetime.datetime.now()
-        snapname = ns.zfsfs+'@'+ns.prefix+'_'+aktuell.isoformat() 
+        snapname = fs+'@'+ns.prefix+'_'+aktuell.isoformat() 
         cmd = 'zfs snapshot '+snapname
         print(cmd)
         aus = os.popen(cmd)
@@ -128,9 +130,9 @@ def main():
             print(j)
         time.sleep(10) 
     def getsnaplist():
-        aus = os.popen('zfs list -H -r -t snapshot -o name '+ns.zfsfs).readlines()
+        aus = os.popen('zfs list -H -r -t snapshot -o name '+fs).readlines()
         # 2. Ausdünnen der Liste um die die nicht den richtigen Prefix haben
-        vgl = ns.zfsfs+'@'+ns.prefix+'_'
+        vgl = fs+'@'+ns.prefix+'_'
         l = len(vgl)
         listesnaps = {}
         for i in aus:
@@ -160,8 +162,10 @@ def main():
                         default=1)
     parser.add_argument('-n','--nodeletedays',dest='nodeletedays',type=int,help='Anzahl Tage an denen regulär nichts gelöscht wird - nach freizuhaltendem Space wird trotzdem gelöscht',
                         default=0)
-    parser.add_argument('-v','--verbose',dest='verbose',action='store_true',help='Macht das Script etwas gesprächiger',default=False)
+    parser.add_argument('-v','--verbose',dest='verbose',action='store_true',help='Macht das Script etwas gesprächiger')
     parser.set_defaults(verbose=False)
+    parser.add_argument('-r','--recursion',dest='recursion',action='store_true',help='Wendet die Einstellungen auch auf alle Filesysteme unterhalb dem übergebenen an')
+    parser.set_defaults(recursion=False)
     ns = parser.parse_args(sys.argv[1:])
     
     inters = []
@@ -169,70 +173,82 @@ def main():
         inter = intervall(i[0],i[1])
         inters.append(inter)
     # 0.1 Cheock ob das FS gemounted ist
-    ret =  checkfs(ns.zfsfs)
-    if ret != True:
-        return ret
+    fslist = []
+    if ns.recursion:
+        # dann sammeln wir mal die Filesysteme
+        liste = os.popen('zfs list -H -r '+ns.zfsfs).readlines()
+        for i in liste:
+            fslist.append(i.split('\t')[0])
+        
+    else:
+        fslist.append(ns.zfsfs)
     
-    # Hier käme dann der Ablauf
-    if ns.dm == 3:
-        # Dann wird nur gecheckt ob genug Platz ist für einen neuen Snapshot
+    for fs in fslist:
+        print('Aktuelles Filesystem:',fs)
+        ret =  checkfs(fs)
+        if ret != 0:
+            continue
+        
+        # Hier käme dann der Ablauf
+        if ns.dm == 3:
+            # Dann wird nur gecheckt ob genug Platz ist für einen neuen Snapshot
+            if checkminfree(True): # Nur wenn genug frei ist, wird ein Snapshot erstellt
+                takeSnapshot()
+            continue
+        # 1. Liste der vorhanden Snapshots
+        listesnaps = getsnaplist()
+        vgl = fs+'@'+ns.prefix+'_'
+        l = len(vgl)
+        
+        heute = datetime.datetime.now()
+        for i in sorted(listesnaps): # Vom ältesten zum neuesten
+            
+            #erstmal die difftage zu heute ermitteln
+            dstring = i[l:]
+            
+            dt = datetime.datetime.strptime(dstring,'%Y-%m-%dT%H:%M:%S.%f')
+            tmp = heute - dt
+            chkday = tmp.days 
+            if ns.verbose:
+                print(i, chkday,'days')
+            hold = False
+            if chkday <= ns.nodeletedays and ns.nodeletedays>0:
+                if ns.verbose:
+                    print('Hold für ',i,'wegen "nodeletedays"')
+                #print(i,'in days',chkday)
+                hold = True
+            else:
+                for x in inters:
+                    if x.checkday(chkday):
+                        if ns.verbose:
+                            print('Hold für ',i,' wegen "interval" days:',x.intervalllaenge,'Anzahl:',x.holdversions )
+                        hold = True
+                        
+                        
+            
+            if hold == False:
+                
+                if checkminfree() == False or ns.dm == 2:
+                    if ns.verbose:
+                        if ns.dm == 2:
+                            print('delete wegen deletemode = 2')
+                        else:
+                            print('delete wegen freespace')
+                    destroySnapshot(i)
+        # Als letztes: Snapshot erstellen
         if checkminfree(True): # Nur wenn genug frei ist, wird ein Snapshot erstellt
             takeSnapshot()
-        return
-    # 1. Liste der vorhanden Snapshots
-    listesnaps = getsnaplist()
-    vgl = ns.zfsfs+'@'+ns.prefix+'_'
-    l = len(vgl)
-    
-    heute = datetime.datetime.now()
-    for i in sorted(listesnaps): # Vom ältesten zum neuesten
-        
-        #erstmal die difftage zu heute ermitteln
-        dstring = i[l:]
-        
-        dt = datetime.datetime.strptime(dstring,'%Y-%m-%dT%H:%M:%S.%f')
-        tmp = heute - dt
-        chkday = tmp.days 
-        if ns.verbose:
-            print(i, chkday,'days')
-        hold = False
-        if chkday <= ns.nodeletedays and ns.nodeletedays>0:
-            if ns.verbose:
-                print('Hold für ',i,'wegen "nodeletedays"')
-            #print(i,'in days',chkday)
-            hold = True
         else:
-            for x in inters:
-                if x.checkday(chkday):
-                    if ns.verbose:
-                        print('Hold für ',i,' wegen "interval" days:',x.intervalllaenge,'Anzahl:',x.holdversions )
-                    hold = True
-                    
-                    
-        
-        if hold == False:
-            
-            if checkminfree() == False or ns.dm == 2:
+            # Dann müssen jetzt noch mehr snaps gelöscht werden - Vom ältesten zum neuesten
+            print('Jetzt wird versucht auf Grund des Speicherplatzes weitere Snapshots zu löschen.')
+            listesnaps = getsnaplist()
+            for i in sorted(listesnaps):
                 if ns.verbose:
-                    if ns.dm == 2:
-                        print('delete wegen deletemode = 2')
-                    else:
-                        print('delete wegen freespace')
+                    print('delete wegen freespace')
                 destroySnapshot(i)
-    # Als letztes: Snapshot erstellen
-    if checkminfree(True): # Nur wenn genug frei ist, wird ein Snapshot erstellt
-        takeSnapshot()
-    else:
-        # Dann müssen jetzt noch mehr snaps gelöscht werden - Vom ältesten zum neuesten
-        print('Jetzt wird versucht auf Grund des Speicherplatzes weitere Snapshots zu löschen.')
-        listesnaps = getsnaplist()
-        for i in sorted(listesnaps):
-            if ns.verbose:
-                print('delete wegen freespace')
-            destroySnapshot(i)
-            if checkminfree(True):
-                takeSnapshot()
-                break
+                if checkminfree(True):
+                    takeSnapshot()
+                    break
     
     print(time.strftime("%Y-%m-%d %H:%M:%S"),APPNAME, VERSION,' ************************** Stop')
 if __name__ == '__main__':
